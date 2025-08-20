8edf68f1a8de7c5019add8c948ac1c5d5bfe4582
Absingh52 <abhisheksingh26460@gmail.com>
2025-08-20 13:52:42 +0530

    Added Day02 practice set

---
+++ Day02/Pratice_set2.py
@@ -0,0 +1,31 @@
+# 1. Write a python program to add two numbers. 
+
+a=18
+b=1
+print("sum of two is :",a+b)
+
+# 2. Write a python program to find remainder when a number is divided by z. 
+
+z=2
+print("remainder when number is diveded by z :",a%z)
+
+# 3. Check the type of variable assigned using input () function. 
+t=float(input("Enter what ever you  want: "))
+print("type of t is:",type(t),t)
+
+# 4. Use comparison operator to find out whether ‘a’ given variable a is greater than ‘b’ or not. Take a = 34 and b = 80
+
+num1=34
+num2=80
+
+compare=(num1>num2)
+print(" num1 given variable a is greater than num2 or not :",compare)
+
+# 5. Write a python program to find an average of two numbers entered by the user. 
+average=(num1+num2)/2
+print("average is :",average)
+
+# 6. Write a python program to calculate the square of a number entered by the user. 
+number=2
+sq=(number*number)
+print("calculate the square of a number entered by the user:",sq)
