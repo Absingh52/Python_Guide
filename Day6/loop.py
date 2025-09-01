ab1a57cf29a887e76ec9ab5dd1e8fac64d45bee3
Absingh52 <abhisheksingh26460@gmail.com>
2025-09-01 16:46:34 +0530

    push

---
+++ Day6/loop.py
@@ -0,0 +1,6 @@
+#   1. Write a program to print multiplication table of a given number using for loop                  
+
+a=int(input("enter the number :"))
+print("hello")
+for i in range(1,11):
+    print( "5 *",i ,"=",a*i)
